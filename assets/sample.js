941f4ad1f34726bcc8a84a203dab343a06af8ca7
JulianF <floydjulian1991@gmail.com>
2022-05-24 14:57:55 -0700

    ..

--- assets/sample.js
+++ assets/sample.js
@@ -1,12 +1,12 @@
 // Remember, we're gonna use strict mode in scripts from now on
-"use strict";
+"use strict"; 
 
 // Problem:
 // We work for a company building a smart home thermometer. Our
 // most recent task is this: "Given an array of temperatures of one day,
 // calculate the temerature amplitude. Sometimes, there might be a sensor error."
 
-const temeratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
 // -What is amplitude? Answer: difference between highest and lowest temp.
@@ -19,20 +19,42 @@ const temeratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // -Find min value in temp array
 // -Subtract min from max (amplitude) and return it.
 
-const calcTempAmplitude = function (temps) {
+const calcTempAmplitudeNew = function (temps1, temps2) {
+    
+    const temps = temps1.concat(temps2);
+
     let max = temps[0];
     let min = temps[0];
+    
     for (let i = 0; i < temps.length; i++) {
-        if(temps[i] > max) {
-            max = temps[i];
+        const curTemp = temps[i];
+        if(typeof curTemp !== 'number') continue;
+        if(curTemp > max) {
+            max = curTemp;
         }
-        if (temps[i] < min) {
-            min = temps[i];
+        if (curTemp < min) {
+            min = curTemp;
         }
     }
-    console.log(max);
+    console.log(max, min);
+    // amplitude is the same as range
+    return max - min;
 };
-    calcTempAmplitude([3, 7, 4]);
+    const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [2, 6, 9]);
+    console.log(amplitudeNew);
     // max = 3
     // is 7 > 3, yes, so now max = 7
     // is 4 > 7 no, so nothing happens, so 7 is max ultimately.
+
+    // Problem 2:
+    // Function should now receive two arrays of temperatures.
+
+    // 1 understanding problem
+    // - With 2 arrays, should we implement functionality twice?
+    // Answer: No. Merge two arrays, but how do we do that?
+
+    // 2 Breaking up into sub-problems (in this case, just one sub-problem).
+    // Look this up on stackoverflow
+    // const array3 = array1.concat(array2);
+
+    
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 // personal-access token: rtZFkbTuxtASTDgIPiCnrQApzZQfZflVQXSnoZkb
 // create variables to store DOM elements below.
- let resultsContainerEl = document.getElementById("results-container");
- let searchBarEl = document.getElementById("search");
- let submitBtnEl = document.getElementById("submit-btn");
- let clearBtnEl = document.getElementById("clear-results");
+let resultsContainerEl = document.getElementById("results-container");
+let searchBarEl = document.getElementById("search");
+let searchFieldEl = document.getElementById("search-field");
+let submitBtnEl = document.getElementById("submit-btn");
+let clearBtnEl = document.getElementById("clear-results");
 
 // discogs data.results[].title = title
 // discogs data.results[].id = id
@@ -16,39 +17,49 @@
  
 
 // upon click, function to render render fetched music data to results-containerEl, display function
-    var displaySearchResults = function () {
-        resultsContainerEl.setAttribute("style", "width: 500px;");          // Rewrite this using DRY principle, you could make this into fewer lines of code.
-        resultsContainerEl.setAttribute("style", "height: 700px;");
-        resultsContainerEl.setAttribute("style", "display: flex;");
-        resultsContainerEl.setAttribute("style", "flex-direction: column;");
-        // create new document.create elements such as headings, cards, imgs, etc. that will all be appended by the results container el and by their parents.
+var displaySearchResults = function () {
+    resultsContainerEl.setAttribute("style", "width: 500px;");          // Rewrite this using DRY principle, you could make this into fewer lines of code.
+    resultsContainerEl.setAttribute("style", "height: 700px;");
+    resultsContainerEl.setAttribute("style", "display: flex;");
+    resultsContainerEl.setAttribute("style", "flex-direction: column;");
+    // create new document.create elements such as headings, cards, imgs, etc. that will all be appended by the results container el and by their parents.
 
-    }
+};
 
 // fetch api data based on search input, use api key with appropriate query and pagination parameters, etc.
-    var getMusicData = function (music) {
-        // "music is a parameter that will be replaced when called with argument in the submit button eventhandler."
-        var discogsUrl = "https://api.discogs.com/database/search?q=&"+ music +"page=10&per_page=25&token=rtZFkbTuxtASTDgIPiCnrQApzZQfZflVQXSnoZkb";
-        fetch(discogsUrl).then(function (response) {
-            response.json().then(function(data) {
-                console.log(data);
-                if (data.results.length === 0) {
-                    alert(error); // do I need to change error to string with error message?
-                } else {
-                    // display music function called here with data passed as argument.
-                }
-            });
+var getMusicData = function (music) {
+    // "music is a parameter that will be replaced when called with argument in the submit button eventhandler."
+    var discogsUrl = "https://api.discogs.com/database/search?q="+ music +"&page=10&per_page=25&token=rtZFkbTuxtASTDgIPiCnrQApzZQfZflVQXSnoZkb";
+    fetch(discogsUrl).then(function (response) {
+        if (!response.ok) {
+            throw Error("ERROR");
+        }
+      response.json().then(function(data) {
+      console.log(data);  // try data.data, then clean up comments
+      })
+      .catch(function (error) {
+          console.log(error)
+      
+      if (data.results.length === 0) {
+            alert(error); // do I need to change error to string with error message?
+
+        } else {
+            displaySearchResults(data);        // display music function called here with data passed as argument.
+            }
         });
-    };
+    });
+};
     // error handling to be added above or below, not sure.
 // function to clear previous searches
 
 // Add eventListener or handler for search button, call top two functions
-    submitBtnEl.addEventListener("click", function(e) {
-        e.preventDefault();
-        console.log('button was clicked');
-        getMusicData(/*pass search text as argument*/)
-    })
+submitBtnEl.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log('button was clicked');
+    var music = searchFieldEl.value;
+    console.log(music);
+    getMusicData(music);
+})
 // Add eventListener or handler for  the clear searches button
 
 // set previous searches to localStorage
@@ -61,4 +72,6 @@
 
 
 // get localStorage on page load
+
+// just to see api data in console log, below.
 getMusicData();
